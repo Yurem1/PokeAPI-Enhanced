5b6fa8720a6cffd9112e227c2b8a9a2ecd6c05a0
Yurem <yuremtovar1@gmail.com>
2024-03-17 15:54:56 -0500

    Add test for fetching a pokemon

---
+++ dist/index.test.js
@@ -0,0 +1,10 @@
+import { Pokedex } from './index';
+// Creates a Pokedex object
+const dex = new Pokedex('ho-oh');
+// If the pokemon entry exists, this test should run normally,
+// Otherwise it will return null & the test will fail.
+test('Fetching a pokemon', async () => {
+    return await dex.getPokemon().then((value) => {
+        expect(value).toBeTruthy();
+    });
+});
