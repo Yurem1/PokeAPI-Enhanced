83221d823e761d633445d3cf3778aadbaac50f9c
Yurem <yuremtovar1@gmail.com>
2024-03-17 01:17:29 -0500

    Refactor Pokedex class and update TypeScript configuration

--- dist/index.js
+++ dist/index.js
@@ -1,17 +1,12 @@
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-import { URL } from './misc.json';
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Pokedex = void 0;
+const misc_json_1 = require("./misc.json");
 /**
  * Represents a Pokedex.
  */
-export class Pokedex {
+class Pokedex {
+    name;
     /**
      * Creates a new instance of the Pokedex class.
      * @param _name - The name of the Pokedex.
@@ -23,23 +18,22 @@ export class Pokedex {
      * Retrieves information about a Pokemon from the API.
      * @returns A Promise that resolves to a Pokemon object, or void if an error occurs.
      */
-    getPokemon() {
-        return __awaiter(this, void 0, void 0, function* () {
-            const url = URL.pokemon;
-            try {
-                // Fetch the pokemon from our name property
-                const req = yield fetch(`${url}${this.name}`, {
-                    method: 'GET',
-                    headers: {
-                        'Accept': 'application/json'
-                    }
-                });
-                // Finally return our Pokémon object.
-                return yield req.json();
-            }
-            catch (error) {
-                return null;
-            }
-        });
+    async getPokemon() {
+        const url = misc_json_1.URL.pokemon;
+        try {
+            // Fetch the pokemon from our name property
+            const req = await fetch(`${url}${this.name}`, {
+                method: 'GET',
+                headers: {
+                    'Accept': 'application/json'
+                }
+            });
+            // Finally return our Pokémon object.
+            return await req.json();
+        }
+        catch (error) {
+            return null;
+        }
     }
 }
+exports.Pokedex = Pokedex;
