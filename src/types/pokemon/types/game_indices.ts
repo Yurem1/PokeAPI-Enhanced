e28735ca91d34b9c8cd5084bb4d552b1291ed249
Yurem <yuremtovar1@gmail.com>
2024-03-16 22:20:28 -0500

    Add descriptions to game_indices and stats types

--- src/types/pokemon/types/game_indices.ts
+++ src/types/pokemon/types/game_indices.ts
@@ -1,7 +1,7 @@
 import { NamedAPIResource } from "../../global_types/global";
 
 /**
- * 
+ * The game version index for this pokemon.
  */
 export default interface VersionGameIndex {
   game_index: number;
--- src/types/pokemon/types/stats.ts
+++ src/types/pokemon/types/stats.ts
@@ -2,6 +2,9 @@ import {
   NamedAPIResource
 } from '../../global_types/global';
 
+/**
+ * The stats for this pokémon.
+ */
 export default interface PokemonStat {
   stat: NamedAPIResource;
   effort: number;
