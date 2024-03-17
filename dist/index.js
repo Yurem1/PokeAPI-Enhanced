69ecef27c1676653e257c06579858341fdd68c9a
Yurem <yuremtovar1@gmail.com>
2024-03-16 20:52:41 -0500

    Delete unused files and dependencies

--- dist/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const pokedex_1 = require("./utils/pokedex");
-async function displayPokemon() {
-    const dex = new pokedex_1.Pokedex('pikachu');
-    const req = await dex.getPokemon();
-    console.log(req?.sprites.other["official-artwork"]);
-}
-displayPokemon().then();
