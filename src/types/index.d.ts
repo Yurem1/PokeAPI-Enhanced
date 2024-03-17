824a718f7ad40e843faec947b7be74fcce9040f5
Yurem <yuremtovar1@gmail.com>
2024-03-16 23:04:19 -0500

    Add new type exports for global types and sprites

---
+++ src/types/index.d.ts
@@ -0,0 +1,11 @@
+export { 
+  NamedAPIResource,
+} from './global_types/global';
+
+export { 
+  DreamWorld,
+  Showdown,
+  Home,
+  OfficialArtwork,
+  PokemonSprites,
+} from './global_types/sprites';
