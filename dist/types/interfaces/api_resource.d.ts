46fcb6723bf2ca1c37d61e2f9fe34e49afed08d4
Yurem <yuremtovar1@gmail.com>
2024-03-17 00:32:03 -0500

    create .d.ts files

---
+++ dist/types/interfaces/api_resource.d.ts
@@ -0,0 +1,13 @@
+/**
+ * Represents a named API resource.
+ */
+export interface NamedAPIResource {
+    /**
+     * The name of the resource.
+     */
+    name: string;
+    /**
+     * The URL of the resource.
+     */
+    url: string;
+}
---
+++ dist/types/interfaces/moves.d.ts
@@ -0,0 +1,31 @@
+import { NamedAPIResource } from "./api_resource";
+/**
+ * Represents the details of a move learned by a Pokémon in a specific version group.
+ */
+export interface PokemonMoveVersion {
+    /**
+     * The method by which the Pokémon learns the move.
+     */
+    move_learn_method: NamedAPIResource;
+    /**
+     * The version group in which the move is learned.
+     */
+    version_group: NamedAPIResource;
+    /**
+     * The level at which the Pokémon learns the move.
+     */
+    level_learned_at: number;
+}
+/**
+ * Represents the moves of a Pokemon.
+ */
+export default interface PokemonMove {
+    /**
+     * The move itself.
+     */
+    move: NamedAPIResource;
+    /**
+     * The details of the move in different version groups.
+     */
+    version_group_details: PokemonMoveVersion[];
+}
