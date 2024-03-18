ef2ff419e18f47fa8d0c4970460b05fc5845b265
Yurem <yuremtovar1@gmail.com>
2024-03-18 03:51:00 -0500

    export pokemon class

--- dist/types/index.d.ts
+++ dist/types/index.d.ts
@@ -3,9 +3,13 @@
  */
 export { Pokedex } from './utility/index';
 /**
+ * Exports from the Pokémon group
+ */
+export { Pokemon } from './utility/Pokemon/pokemon';
+/**
  * Export every entity from the Pokemon type
  */
-export { Pokemon } from './interfaces/Pokemon/pokemon';
+export { IPokemon } from './interfaces/Pokemon/pokemon';
 export * from './interfaces/Pokemon/Pokemon/abilities';
 export * from './interfaces/Pokemon/Pokemon/cries';
 export * from './interfaces/Pokemon/Pokemon/game_indices';
