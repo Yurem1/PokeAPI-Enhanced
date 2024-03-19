c51f558102cde451936f607396b7f2681f1bff6b
Yurem <yuremtovar1@gmail.com>
2024-03-19 00:55:57 -0500

    misc test

--- dist/index.test.js
+++ dist/index.test.js
@@ -2,6 +2,7 @@ import { Pokedex } from './utility/index';
 // If the pokemon entry exists, this test should run normally,
 // Otherwise it will return null & the test will fail.
 test('Fetching a pokemon', async () => {
-    const dex = await Pokedex.getPokemon('mr. mime.');
+    const dex = await Pokedex.getNature('adamant');
+    console.log(dex?.getHatesFlavor?.name);
     expect(dex).toBeTruthy();
 });
