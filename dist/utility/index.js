3acd4df80fe57b318baa562afc7854fd385584a3
Yurem <yuremtovar1@gmail.com>
2024-03-19 00:56:12 -0500

    Add Nature class and getNature method

--- dist/utility/index.js
+++ dist/utility/index.js
@@ -1,4 +1,5 @@
 import URL from '../misc.json';
+import { Nature } from './Pokemon/nature';
 import { Pokemon } from './Pokemon/pokemon';
 /**
  * Represents a Pokedex.
@@ -9,6 +10,10 @@ export class Pokedex {
      * The URL endpoint https://pokeapi.co/api/v2/pokemon/
      */
     POKEMON_URL = URL.ENDPOINTS.pokemon;
+    /**
+     * The URL endpoint https://pokeapi.co/api/v2/nature/
+     */
+    NATURE_URL = URL.ENDPOINTS.nature;
     /**
      * Creates a new instance of the Pokédex class.
      * @param _name - The name of the Pokédex entry.
@@ -24,7 +29,7 @@ export class Pokedex {
     }
     /**
      * Retrieves information about a Pokemon from the API.
-     * @returns A Promise that resolves to a Pokemon object, or null if an error occurs.
+     * @returns A Promise that resolves to a **Pokemon** object, or null if an error occurs.
      */
     static async getPokemon(_name) {
         // The pokedex entry.
@@ -39,6 +44,29 @@ export class Pokedex {
             });
             // Parse the response as JSON and return it as a Pokemon object.
             return new Pokemon(await req.json());
+        }
+        catch {
+            // If an error occurs during the API request, return null.
+            return null;
+        }
+    }
+    /**
+     * Retrieves information about a Nature from the API.
+     * @returns A Promise that resolves to a **Nature** object, or null if an error occurs.
+     */
+    static async getNature(_name) {
+        // The pokedex entry.
+        const entry = new Pokedex(_name);
+        try {
+            // Fetch the pokemon data from the API using the specified name
+            const req = await fetch(`${entry.NATURE_URL}${entry.name}`, {
+                method: 'GET',
+                headers: {
+                    'Accept': 'application/json'
+                }
+            });
+            // Parse the response as JSON and return it as a Nature object.
+            return new Nature(await req.json());
         }
         catch {
             // If an error occurs during the API request, return null.
--- src/utility/index.ts
+++ src/utility/index.ts
@@ -1,4 +1,5 @@
 import URL from '../misc.json';
+import { Nature } from './Pokemon/nature';
 import { Pokemon } from './Pokemon/pokemon';
 
 /**
@@ -11,6 +12,11 @@ export class Pokedex {
    * The URL endpoint https://pokeapi.co/api/v2/pokemon/
    */
   private readonly POKEMON_URL = URL.ENDPOINTS.pokemon;
+
+  /**
+   * The URL endpoint https://pokeapi.co/api/v2/nature/
+   */
+  private readonly NATURE_URL = URL.ENDPOINTS.nature;
 
   /**
    * Creates a new instance of the Pokédex class.
@@ -28,7 +34,7 @@ export class Pokedex {
 
   /**
    * Retrieves information about a Pokemon from the API.
-   * @returns A Promise that resolves to a Pokemon object, or null if an error occurs.
+   * @returns A Promise that resolves to a **Pokemon** object, or null if an error occurs.
    */
   static async getPokemon(_name: string): Promise<Pokemon | null> {
     // The pokedex entry.
@@ -45,6 +51,32 @@ export class Pokedex {
 
       // Parse the response as JSON and return it as a Pokemon object.
       return new Pokemon(await req.json());
+
+    } catch {
+      // If an error occurs during the API request, return null.
+      return null;
+    }
+  }
+
+  /**
+   * Retrieves information about a Nature from the API.
+   * @returns A Promise that resolves to a **Nature** object, or null if an error occurs.
+   */
+  static async getNature(_name: string): Promise<Nature | null> {
+    // The pokedex entry.
+    const entry = new Pokedex(_name);
+
+    try {
+      // Fetch the pokemon data from the API using the specified name
+      const req = await fetch(`${entry.NATURE_URL}${entry.name}`, {
+        method: 'GET',
+        headers: {
+          'Accept': 'application/json'
+        }
+      });
+
+      // Parse the response as JSON and return it as a Nature object.
+      return new Nature(await req.json());
 
     } catch {
       // If an error occurs during the API request, return null.
