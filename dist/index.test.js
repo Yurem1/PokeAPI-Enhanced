edeef684651c98adb02987908dc81d8a9098edd8
Yurem <yuremtovar1@gmail.com>
2024-03-19 01:06:50 -0500

    remove old compiled files

--- dist/index.test.js
+++
@@ -1,8 +0,0 @@
-import { Pokedex } from './utility/index';
-// If the pokemon entry exists, this test should run normally,
-// Otherwise it will return null & the test will fail.
-test('Fetching a pokemon', async () => {
-    const dex = await Pokedex.getNature('adamant');
-    console.log(dex?.getHatesFlavor?.name);
-    expect(dex).toBeTruthy();
-});
