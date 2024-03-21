10db9a96380289a9c0b73be85002322896b9c572
Yurem <yuremtovar1@gmail.com>
2024-03-20 21:32:56 -0500

    new test

--- dist/index.test.js
+++ dist/index.test.js
@@ -3,6 +3,5 @@ import { Pokedex } from './utility/pokedex';
 // Otherwise it will return null & the test will fail.
 test('Fetching a pokemon', async () => {
     const dex = await Pokedex.getNature('adamant');
-    console.log(dex?.getName);
     expect(dex).toBeTruthy();
 });
