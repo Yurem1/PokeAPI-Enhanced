ab877a7e0cb29b8b9d960280f059b6b2fec14712
Yurem <yuremtovar1@gmail.com>
2024-03-22 22:36:11 -0500

    update index

--- dist/index.js
+++ dist/index.js
@@ -7,6 +7,7 @@ export { Pokedex } from './utility/pokedex';
  */
 export { Pokemon } from './utility/Pokemon/pokemon';
 export { Nature } from './utility/Pokemon/nature';
+export { Abilities } from './utility/Pokemon/abilities';
 // IPokemon
 export * from './interfaces/Pokemon/Pokemon/abilities';
 export * from './interfaces/Pokemon/Pokemon/cries';
@@ -17,7 +18,13 @@ export * from './interfaces/Pokemon/Pokemon/past_types';
 export * from './interfaces/Pokemon/Pokemon/sprites';
 export * from './interfaces/Pokemon/Pokemon/stats';
 // INature
-export * from './interfaces/Pokemon/Natures/encounter';
+export * from './interfaces/Pokemon/Pokemon Location Areas/encounter';
 export * from './interfaces/Pokemon/Natures/move_battle_style_preferences';
 export * from './interfaces/Pokemon/Natures/name';
 export * from './interfaces/Pokemon/Natures/pokeathlon_stat_changes';
+// IAbilities
+export * from './interfaces/Pokemon/Abilities/effect';
+export * from './interfaces/Pokemon/Abilities/effect_changes';
+export * from './interfaces/Pokemon/Abilities/effect_entries';
+export * from './interfaces/Pokemon/Abilities/flavor_text_entries';
+export * from './interfaces/Pokemon/Abilities/pokemon';
--- dist/types/index.d.ts
+++ dist/types/index.d.ts
@@ -7,11 +7,13 @@ export { Pokedex } from './utility/pokedex';
  */
 export { Pokemon } from './utility/Pokemon/pokemon';
 export { Nature } from './utility/Pokemon/nature';
+export { Abilities } from './utility/Pokemon/abilities';
 /**
  * Export every entity from the Pokemon type
  */
 export { IPokemon } from './interfaces/Pokemon/pokemon';
 export { INature } from './interfaces/Pokemon/nature';
+export { IAbilities } from './interfaces/Pokemon/abilities';
 export * from './interfaces/Pokemon/Pokemon/abilities';
 export * from './interfaces/Pokemon/Pokemon/cries';
 export * from './interfaces/Pokemon/Pokemon/game_indices';
@@ -20,10 +22,15 @@ export * from './interfaces/Pokemon/Pokemon/moves';
 export * from './interfaces/Pokemon/Pokemon/past_types';
 export * from './interfaces/Pokemon/Pokemon/sprites';
 export * from './interfaces/Pokemon/Pokemon/stats';
-export * from './interfaces/Pokemon/Natures/encounter';
+export * from './interfaces/Pokemon/Pokemon Location Areas/encounter';
 export * from './interfaces/Pokemon/Natures/move_battle_style_preferences';
 export * from './interfaces/Pokemon/Natures/name';
 export * from './interfaces/Pokemon/Natures/pokeathlon_stat_changes';
+export * from './interfaces/Pokemon/Abilities/effect';
+export * from './interfaces/Pokemon/Abilities/effect_changes';
+export * from './interfaces/Pokemon/Abilities/effect_entries';
+export * from './interfaces/Pokemon/Abilities/flavor_text_entries';
+export * from './interfaces/Pokemon/Abilities/pokemon';
 /**
  * Export NamedAPIResource (found in almost all types)
  */
