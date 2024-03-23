66597f8ee3be997520ed3b5adabd9e5cdcf47215
Yurem <yuremtovar1@gmail.com>
2024-03-23 12:39:15 -0500

    add comments to this class

--- src/utility/Pokemon/nature.ts
+++ src/utility/Pokemon/nature.ts
@@ -4,6 +4,15 @@ import { NatureStateChange } from '../../interfaces/Pokemon/Natures/pokeathlon_s
 import { INature } from '../../interfaces/Pokemon/nature';
 import { NamedAPIResource } from '../../interfaces/api_resource';
 
+/**
+ * @class
+ * A comprehensive representation of a Pokémon's nature, containing all the resources available in the Pokémon API.
+ * This class encapsulates data such as the nature's ID, name, decreased and increased stats, flavors it likes and hates, 
+ * pokeathlon stat changes, move battle style preferences, and more.
+ * It provides convenient getter methods to access each attribute of the Pokémon's nature.
+ * 
+ * Use this class to easily retrieve and manipulate Pokémon nature data from the https://pokeapi.co API.
+ */
 export class Nature {
   private readonly id: number | null;
   private readonly name: string | null;
@@ -28,63 +37,72 @@ export class Nature {
   }
 
   /**
-   * Getter for the Pokémon ID
+   * Gets the pokémon ID.
+   * @return The pokémon ID, or null if not available.
    */
   public get getPokemonID(): number | null {
     return this.id;
   }
 
   /**
-   * Getter for the nature name
+   * Gets the nature name.
+   * @return The nature name, or null if not available.
    */
   public get getName(): string | null {
     return this.name;
   }
 
   /**
-   * Getter for the stat that is decreased by this nature
+   * Gets the stat that is decreased by this nature.
+   * @return The decreased stat, or null if not available.
    */
   public get getDecreaseStat(): NamedAPIResource | null {
     return this.decreased_stat;
   }
 
   /**
-   * Getter for the stat that is increased by this nature
+   * Gets the stat that is increased by this nature.
+   * @return The increased stat, or null if not available.
    */
   public get getIncreasedStat(): NamedAPIResource | null {
     return this.increased_stat;
   }
 
   /**
-   * Getter for the flavor that the Pokémon hates when this nature is active
+   * Gets the flavor that the Pokémon hates when this nature is active.
+   * @return The flavor that the Pokémon hates, or null if not available.
    */
   public get getHatesFlavor(): NamedAPIResource | null {
     return this.hates_flavor;
   }
 
   /**
-   * Getter for the flavor that the Pokémon likes when this nature is active
+   * Gets the flavor that the Pokémon likes when this nature is active.
+   * @return The flavor that the Pokémon likes, or null if not available.
    */
   public get getLikesFlavor(): NamedAPIResource | null {
     return this.likes_flavor;
   }
 
   /**
-   * Getter for the stat changes that occur to the Pokémon when participating in Pokéathlon
+   * Gets the stat changes that occur to the Pokémon when participating in Pokéathlon.
+   * @return The stat changes for Pokéathlon, or null if not available.
    */
   public get getPokeathlonStatChanges(): NatureStateChange[] | null {
     return this.pokeathlon_stat_changes;
   }
 
   /**
-   * Getter for the move battle style preferences for this nature
+   * Gets the move battle style preferences for this nature.
+   * @return The move battle style preferences, or null if not available.
    */
   public get getMoveBattleStylePreferences(): MoveBattleStylePreference[] | null {
     return this.move_battle_style_preferences;
   }
 
   /**
-   * Getter for the localized names for this nature
+   * Gets the localized names for this nature.
+   * @return The localized names, or null if not available.
    */
   public get getNames(): Name | null {
     return this.names;
